f9f499e50dbde93d3e24484633c3ad93ffc5fd8f
Mohammed <74480856+MohammedSaleh2001@users.noreply.github.com>
2022-10-31 12:16:54 -0600

    WILL FAIL LOL

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,10 +1,13 @@
 package com.example.lab8;
 
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 //import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -33,6 +36,15 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+
+    @Test
+    void testHasCity() {
+        list = MockCityList();
+        City city = new City("Charlottetown", "Prince Edward Island");
+        assertFalse(list.hasCity(city));
+        list.add(city);
+        Assertions.assertTrue(list.hasCity(city));
     }
 
 
